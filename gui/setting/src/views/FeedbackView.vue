140951e197a7a90c57475ddc6c2e316daf75db4b
XuZiheng <xu_ziheng@outlook.com>
2025-04-09 21:56:29 +0800

    Remove contact information section from FeedbackView.vue

--- gui/setting/src/views/FeedbackView.vue
+++ gui/setting/src/views/FeedbackView.vue
@@ -8,19 +8,7 @@
         <n-h3 style="margin-bottom: 0;">使用反馈</n-h3>
       </n-alert>
 
-    <n-card title="🌐 联系方式" embedded :bordered="true">
-      <n-space size="small" style="margin-top: 10px">
-        <n-tag :bordered="false" type="info" size="small">Email:</n-tag>lyf200@qq.com<br>
-      </n-space>
-      <n-space size="small" style="margin-top: 10px">
-        <n-tag :bordered="false" type="info" size="small">微信号:</n-tag>upsort<br>
-      </n-space>
-      <n-space size="small" style="margin-top: 10px">
-        <n-tag :bordered="false" type="info" size="small">Telegram:</n-tag>
-        <a target="_blank" href="https://t.me/shaziaicom">https://t.me/shaziaicom</a><br>
-      </n-space>
-      <n-p style="color: #666;">微信加群请先添+wx，需备注：Tuboshu </n-p>
-    </n-card>
+
   </div>
 </template>
 
