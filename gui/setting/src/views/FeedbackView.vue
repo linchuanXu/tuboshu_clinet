b3e25d31a0b347b697f0f83264aa020118e1d362
XU <xu_ziheng@outlook.com>
2025-04-23 09:44:50 +0800

    ✨ feat(FeedbackView): 更新反馈页面内容和结构
    - 修改标题从“使用反馈”为“关于MindNotes”
    - 替换卡片标题为“项目说明”，移除Telegram联系方式
    - 增加项目简介描述【内容】
    - 调整联系信息排版，移除微信群加入说明【布局】

--- gui/setting/src/views/FeedbackView.vue
+++ gui/setting/src/views/FeedbackView.vue
@@ -5,21 +5,23 @@
 <template>
   <div id="content-main">
       <n-alert :show-icon="false" type="info" style="margin-bottom: 1rem;">
-        <n-h3 style="margin-bottom: 0;">使用反馈</n-h3>
+        <n-h3 style="margin-bottom: 0;">关于MindNotes</n-h3>
       </n-alert>
 
-    <n-card title="🌐 联系方式" embedded :bordered="true">
+    <n-card title="📝 项目说明" embedded :bordered="true">
+      <n-p style="color: #666;">
+        MindNotes是基于Tuboshu二次开发的知识管理工具，专注于提升个人知识管理效率。
+      </n-p>
+      <n-p style="color: #666; margin-top: 10px;">
+        如果您有任何建议或问题，可以通过以下方式Tuboshu开发团队：
+      </n-p>
+      
       <n-space size="small" style="margin-top: 10px">
         <n-tag :bordered="false" type="info" size="small">Email:</n-tag>lyf200@qq.com<br>
       </n-space>
       <n-space size="small" style="margin-top: 10px">
         <n-tag :bordered="false" type="info" size="small">微信号:</n-tag>upsort<br>
       </n-space>
-      <n-space size="small" style="margin-top: 10px">
-        <n-tag :bordered="false" type="info" size="small">Telegram:</n-tag>
-        <a target="_blank" href="https://t.me/shaziaicom">https://t.me/shaziaicom</a><br>
-      </n-space>
-      <n-p style="color: #666;">微信加群请先添+wx，需备注：Tuboshu </n-p>
     </n-card>
   </div>
 </template>
